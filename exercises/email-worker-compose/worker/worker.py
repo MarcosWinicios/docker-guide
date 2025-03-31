e3cba49d27698276eac7c36e7a0c4a2ac71fb9b8
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-03-31 02:10:28 -0300

    exercises(email-worker): Add multiple instances of the worker service

--- exercises/email-worker-compose/worker/worker.py
+++ exercises/email-worker-compose/worker/worker.py
@@ -5,6 +5,7 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Aguardando mensagens na fila...')
     while True:
         mensgem = json.loads(r.blpop('sender')[1])
         ## Simular o envio de email...
